cac8b4a27cbb334c5b5b651e5a90e75c120b70a9
Teodora Tica <ticateodoraioana@gmail.com>
2018-12-02 22:58:40 +0200

    Added form binding and eligibility checking

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Router } from '@angular/router';
+import { NgForm, FormBuilder, Validators, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -12,13 +13,46 @@ export class FormComponent implements OnInit {
 
   canDonate = false;
   formCompleted = false;
-  constructor(public afAuth: AngularFireAuth, private db: AngularFireDatabase, private router: Router) { }
+  diseases: string[] = [];
+  otherRestrictions: string[] = [];
+  donationForm: FormGroup;
+
+  constructor(
+    public afAuth: AngularFireAuth, 
+    private db: AngularFireDatabase, 
+    private router: Router, 
+    private formBuilder: FormBuilder,) { }
 
   ngOnInit() {
+    this.donationForm = this.formBuilder.group({
+      age: ['', Validators.required],
+      weight: ['', Validators.required],
+      gender: ['', Validators.required],
+      bloodType: ['', Validators.required],
+      rh: ['', Validators.required]
+    });
+  }
+
+  get f() { return this.donationForm.controls; }
+
+  isEligible() {
+    console.log(this.f.weight.value);
+
+    if (parseInt(this.f.weight.value) < 50) {
+      return false;
+    } else if (parseInt(this.f.age.value) < 18 ) {
+      return false;
+    } else if (this.diseases.length != 0) {
+      return false;
+    } else if (this.otherRestrictions.length != 0) {
+      return false;
+    }
+
+    return true;
   }
 
   submit() {
-    this.canDonate = true;
+    this.canDonate = this.isEligible();
     this.formCompleted = true;
   }
 
@@ -38,5 +72,31 @@ export class FormComponent implements OnInit {
     }
     this.db.list('donations/' + uid + '/').push(data);
     this.router.navigate(['/user/results']);
+  }
+
+  toggleDisease(disease: string) {
+    let index = this.diseases.indexOf(disease);
+    
+    if (index == -1) {
+      this.diseases.push(disease);
+    } else {
+      this.diseases.splice(index, 1);
+    }
+  }
+
+  addRestriction(restriction: string) {
+    let index = this.otherRestrictions.indexOf(restriction);
+  
+    if (index == -1) {
+      this.otherRestrictions.push(restriction);
+    } 
+  }
+
+  removeRestriction(restriction: string) {
+    let index = this.otherRestrictions.indexOf(restriction);
+  
+    if (index != -1) {
+      this.otherRestrictions.splice(index, 1);
+    } 
   }
 }
